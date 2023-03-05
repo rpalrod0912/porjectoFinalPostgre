0c8e1276b450f59095ce1c6ea0e8c9a30fb99fa5
rpalrod0912 <rpalrod0912@g.educaand.es>
2023-03-05 13:56:34 +0100

    Testing!

--- src/test/java/com/example/porjectofinalpostgre/PorjectoFinalPostgreApplicationTests.java
+++ src/test/java/com/example/porjectofinalpostgre/PorjectoFinalPostgreApplicationTests.java
@@ -1,13 +1,57 @@
 package com.example.porjectofinalpostgre;
 
+import com.example.porjectofinalpostgre.Controller.UserController;
+import com.example.porjectofinalpostgre.Repository.OrderItemRepository;
+import com.example.porjectofinalpostgre.Repository.OrderRepository;
+import com.example.porjectofinalpostgre.Repository.ProductRepository;
+import com.example.porjectofinalpostgre.Repository.UserRepository;
+import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class PorjectoFinalPostgreApplicationTests {
+
+
+	@Autowired
+	MockMvc mvc;
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@Autowired
+	OrderRepository orderRepository;
+
+	@Autowired
+	OrderItemRepository orderItemRepository;
+
+	@Autowired
+	UserController userController;
 
 	@Test
 	void contextLoads() {
+	assert userRepository.count()==3;
+	assert productRepository.count()==6;
+	assert orderRepository.count()==4;
 	}
+
+	@Test
+	void getAll() throws Exception{
+		mvc.perform()
+	}
+
+
+
 
 }
